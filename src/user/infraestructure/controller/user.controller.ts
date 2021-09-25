d7607c381c54c43fe2f5d4290e995b7f0070a491
David <cruz-azul99@hotmail.com>
2021-09-25 09:47:36 -0500

    Get method created

--- src/user/infraestructure/controller/user.controller.ts
+++ src/user/infraestructure/controller/user.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
 import { UnitOfWork } from 'src/share/unit-of-work/unit-of-work.provider';
 import { IBaseCreateUser } from 'src/user/application/interfaces/base.create.user';
+import { IBaseGetUser } from 'src/user/application/interfaces/base.get.users';
 import { CreateUserRequestDto } from 'src/user/dto/create.request.dto';
 import { CreateUserResponseDto } from 'src/user/dto/create.response.dto.';
 import { USER_TYPES } from 'src/user/user.constants';
@@ -8,6 +9,7 @@ import { USER_TYPES } from 'src/user/user.constants';
 @Controller('user')
 export class UserController {
   constructor(@Inject(USER_TYPES.BaseCreateUser) private readonly _baseCreateUser: IBaseCreateUser,
+  @Inject(USER_TYPES.BaseGetUser) private readonly _baseGetUser: IBaseGetUser,
     private uow: UnitOfWork
   ){}
 
@@ -15,5 +17,11 @@ export class UserController {
   async createUser(@Body() userDto: CreateUserRequestDto): Promise<CreateUserResponseDto> {
     const saved = await this.uow.withTransaction(() => this._baseCreateUser.createUser(userDto));
     return saved;
+  }
+
+  @Get()
+  async getUsers(): Promise<CreateUserResponseDto[]> {
+    const users = await this.uow.withTransaction(() => this._baseGetUser.getUsers());
+    return users;
   }
 }
